ff35579cf499273f5214ca0052a6a118081053f1
Joy Sarker <joysarker39@gmail.com>
2021-09-30 00:23:57 +0600

    Update DC motor with keypad.ino

--- Assignment 2/DC motor with keypad.ino
+++ Assignment 2/DC motor with keypad.ino
@@ -1,4 +1,5 @@
 #include <Keypad.h>
+#define dw digitalWrite
 
 const byte ROWS = 4; // Four rows
 const byte COLS = 4; // Four columns as this is a 4*4 Keypad
@@ -15,84 +16,61 @@ char keys[ROWS][COLS] = {
 
 }; // defining the keys in the keypad
 
-byte rowPins[ROWS] = { 8, 7, 5, 4 };// Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
-byte colPins[COLS] = { 3, 2, 1, 0 }; // Connect keypad COL0, COL1, COL2 and COL3 to these Arduino pins.
+//here 13, 12, 8, 7 pins are disconnected
+byte rowPins[ROWS] = {13, 12, 4, 8}; // Connect keypad ROW0, ROW1, ROW2 and ROW3 to these Arduino pins.
+byte colPins[COLS] = {2, 1, 0, 7}; // Connect keypad COL0, COL1, COL2 and COL3 to these Arduino pins.
 
 Keypad kpd = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS ); // Create the Keypad variable to identify and store the pressed key.
 
+struct pin {
+  int p1 = 0;
+  int p2 = 0;
+  pin(int _a = 0, int _b = 0) {
+    p1 = _a;
+    p2 = _b;
+  }
+}; /// Motor Pin Structure
 
-
-int EN1 = 13;
-int EN2 = 12;
+pin DCmotor[4] = {{0, 0}, {11, 10}, {9, 6}, {5, 3}}; /// storing every motor pin number
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(EN1, OUTPUT);
-  pinMode(EN2, OUTPUT);
-  pinMode(6, OUTPUT);
-  pinMode(9, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(11, OUTPUT);
+  for (int i = 0; i < 4; i++) {
+    pinMode(DCmotor[i].p1, OUTPUT);
+    pinMode(DCmotor[i].p2, OUTPUT);
+  } // setting up every pin mode
 }
 
-char KEY = 'n';
+void clockwise(pin motor){
+  dw(motor.p1, HIGH);
+  dw(motor.p2, LOW);
+}/// clockwise motor rotation
+
+void anti_clockwise(pin motor){
+  dw(motor.p2, HIGH);
+  dw(motor.p1, LOW);
+}//anti clockwise motor rotation
+
+void off(pin motor){
+  dw(motor.p1, LOW);
+  dw(motor.p2, LOW);
+}//motor turn off
+
+int motorNo = 0; // indicates which motor should rotate
 
 void loop() {
-// put your main code here, to run repeatedly:
-digitalWrite(EN1, HIGH);
-digitalWrite(EN2, HIGH);
-char key = kpd.getKey(); /// get key from keypad
-if (key != 0) {
-if (KEY == 'n') {
-KEY = key;
-} else {
-if (key == '1') {
-  if (KEY == '1') {
-    digitalWrite(11, HIGH);
-    digitalWrite(10, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(6, LOW);
-    delay(2000);
-    KEY = 'n';
-    digitalWrite(11, LOW);
-  } else if (KEY == '2') {
-    digitalWrite(11, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(9, HIGH);
-    digitalWrite(6, LOW);
-    delay(2000);
-    digitalWrite(9, LOW);
-    KEY = 'n';
-  } else if (KEY == '3') {
-    // digitalWrite(11, HIGH);
-    // digitalWrite(10, LOW);
-    //digitalWrite(9, LOW);
-    //digitalWrite(6, LOW);
+  char key = kpd.getKey(); /// get key from keypad
+  for(int i = 1; i < 4; i++){
+    off(DCmotor[i]); /// initially turn off every motor
   }
-} else {
-  if (KEY == '1') {
-    digitalWrite(11, LOW);
-    digitalWrite(10, HIGH);
-    digitalWrite(9, LOW);
-    digitalWrite(6, LOW);
-    delay(2000);
-    digitalWrite(10, LOW);
-    KEY = 'n';
-  } else if (KEY == '2') {
-    digitalWrite(11, LOW);
-    digitalWrite(10, LOW);
-    digitalWrite(9, LOW);
-    digitalWrite(6, HIGH);
-    delay(2000);
-    KEY = 'n';
-    digitalWrite(6, LOW);
-} else if (KEY == '3') {
-    // digitalWrite(11, HIGH);
-    // digitalWrite(10, LOW);
-    //digitalWrite(9, LOW);
-    // digitalWrite(6, LOW);
+  if (key != 0) {
+    if (key >= '1' and key <= '3') { 
+      if (!motorNo) motorNo = key - '0'; /// checking if 1st digit entered
+      else if(key >= '1' and key <='2') {
+        if (key == '1') clockwise(DCmotor[motorNo]);
+        if (key == '2') anti_clockwise(DCmotor[motorNo]);
+        delay(5000);
+        motorNo = 0;
+      }
+    }
   }
-}
-}
-}
 }
